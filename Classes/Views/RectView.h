783aa4696484ba24506835bc03207218bd2835b3
Tim <ma3tsa@mail.ru>
2014-01-13 14:31:45 +0400

    Declaring Rect View Class

---
+++ Classes/Views/RectView.h
@@ -0,0 +1,13 @@
+//
+//  RectView.h
+//  Quartz Drawings
+//
+//  Created by Ultimatum on 13.01.14.
+//  Copyright (c) 2014 Timur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RectView : UIView
+
+@end
---
+++ Classes/Views/RectView.m
@@ -0,0 +1,37 @@
+//
+//  RectView.m
+//  Quartz Drawings
+//
+//  Created by Ultimatum on 13.01.14.
+//  Copyright (c) 2014 Timur. All rights reserved.
+//
+
+#import "RectView.h"
+
+@interface RectView () {
+    BOOL _drawFill;
+    BOOL _drawFillInnerShadow;
+    BOOL _drawFillOuterShadow;
+    BOOL _drawStroke;
+}
+
+@end
+
+@implementation RectView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+/*
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
