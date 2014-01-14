7aebfeb625944573f5d12e6dbf4583c97bb61d76
Tim <ma3tsa@mail.ru>
2014-01-14 15:34:23 +0400

    added arc drawing code
    transferred filling, inner and outer shading, stroking in a separate
    UIView class category

--- Quartz Drawings/AppDelegate.m
+++ Quartz Drawings/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "RectView.h"
+#import "ExampleView.h"
 
 @implementation AppDelegate
 
@@ -18,13 +18,9 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    RectView *view = [[RectView alloc] initWithFrame: CGRectInset(self.window.bounds, 110, 110)];
+    ExampleView *view = [[ExampleView alloc] initWithFrame: CGRectInset(self.window.bounds, 100, 100)];
     [self.window addSubview: view];
-    
 
-    //on a dev branch
-    //will add some images
-    
     return YES;
 }
 
