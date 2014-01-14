2cc50f2c5bd5f054e8dee438489ad3be1d6c3475
Tim <ma3tsa@mail.ru>
2014-01-14 13:36:36 +0400

    00

--- Quartz Drawings/AppDelegate.m
+++ Quartz Drawings/AppDelegate.m
@@ -3,7 +3,7 @@
 //  Quartz Drawings
 //
 //  Created by Ultimatum on 13.01.14.
-//  Copyright (c) 2014 Timur. All rights reserved.
+//  Copyright (c) 2014 Timur. All rights reserved. 
 //
 
 #import "AppDelegate.h"
