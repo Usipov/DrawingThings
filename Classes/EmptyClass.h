f612e343b0961c3ce7f9978fafbe126cc280e842
Tim <ma3tsa@mail.ru>
2014-01-15 11:01:17 +0400

    added an empty class, added a group, added a subgroup

---
+++ Classes/EmptyClass.h
@@ -0,0 +1,13 @@
+//
+//  EmptyClass.h
+//  Quartz Drawings
+//
+//  Created by Ultimatum on 15.01.14.
+//  Copyright (c) 2014 Timur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmptyClass : NSObject
+
+@end
---
+++ Classes/EmptyClass.m
@@ -0,0 +1,13 @@
+//
+//  EmptyClass.m
+//  Quartz Drawings
+//
+//  Created by Ultimatum on 15.01.14.
+//  Copyright (c) 2014 Timur. All rights reserved.
+//
+
+#import "EmptyClass.h"
+
+@implementation EmptyClass
+
+@end
