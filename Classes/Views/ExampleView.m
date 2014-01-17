7a31b29fa4ad15c934b882b1ce087b60726fcb4d
Tim <ma3tsa@mail.ru>
2014-01-17 10:02:06 +0400

    conflicts fixed

--- Classes/Views/ExampleView.m
+++ Classes/Views/ExampleView.m
@@ -37,63 +37,7 @@
     UIBezierPath* path = [self rectPathInRect: rectToDrawIn];
     //UIBezierPath* path = [self circlePathInRect: rectToDraw];
     
-<<<<<<< HEAD
-    //circle path
-    {
-        
-        //path = [UIBezierPath bezierPathWithOvalInRect: rectToDraw];
-        
-        CGFloat circleRadius = 0.5 * MIN(CGRectGetWidth(rectToDraw), CGRectGetHeight(rectToDraw));
 
-        path = [UIBezierPath bezierPath];
-        [path addArcWithCenter: CGPointMake(0, 0) radius: circleRadius startAngle: 120 * M_PI/180 endAngle: 350 * M_PI/180 clockwise: YES];
-        [path addLineToPoint: CGPointMake(0, 0)];
-        [path closePath];
-        
-        CGAffineTransform ovalTransform = CGAffineTransformMakeTranslation(CGRectGetMidX(rectToDraw), CGRectGetMidY(rectToDraw));
-        ovalTransform = CGAffineTransformScale(ovalTransform, 1, CGRectGetHeight(rectToDraw) / CGRectGetWidth(rectToDraw));
-        [path applyTransform: ovalTransform];
-    }
-
-    
-    
-    //draw, fill, add outer shadow
-    if (_needToFill)
-    {
-        UIColor* fillColor = [UIColor colorWithRed: 0.531 green: 0.114 blue: 0.705 alpha: 1];
-        CGSize outerShadowOffset = CGSizeMake(3.1, 3.1);
-        CGFloat outerShadowBlurRadius = 10;
-        UIColor* outerShadowColor = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 1];
-        
-        if (! _needToAddOuterShadow) {
-            [self drawPath: path filledWithColor: fillColor];
-        } else {
-            [self drawPath: path filledWithColor: fillColor outerShadowColor: outerShadowColor shadowOffset: outerShadowOffset shadowBlurRadius: outerShadowBlurRadius inContext: context];
-        }
-    }
-
-    //add inner shadow
-    if (_needToAddInnerShadow)
-    {
-        CGSize innerShadowOffset = CGSizeMake(0.1, 0.1);
-        CGFloat innerShadowBlurRadius = 15;
-        UIColor* innerShadowColor = [UIColor colorWithRed: 0.041 green: 0.001 blue: 0.001 alpha: 1];
-        
-        [self drawInnerShadowForPath: path innerShadowColor: innerShadowColor shadowOffset: innerShadowOffset shadowBlurRadius: innerShadowBlurRadius inContext:context];
-    }
-    
-    //add stroke
-    if (_needToStroke)
-    {
-        UIColor* strokeColor = [UIColor colorWithRed: 0 green: 0 blue: 0.114 alpha: 1];
-        CGFloat strokeLineWidth = 2.0f;
-        CGFloat strokePattern[] = {6, 3};
-        NSInteger strokePatternCount = 2;
-        CGFloat strokePhase = 0.0f;
-        [self drawPath: path strokedWithColor: strokeColor lineWidth: strokeLineWidth lineDash: strokePattern count: strokePatternCount phase: strokePhase];
-    }
-=======
-    
     //draw
     //[self fillPath: path];
     //[self fillAndOuterShadePath: path];
@@ -103,7 +47,6 @@
     [self innerShadePath: path];
     [self strokePath: path];
     
->>>>>>> update-branch
 }
 
 #pragma mark - paths
